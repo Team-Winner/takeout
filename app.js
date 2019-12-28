b11e046f5f3ad7ad1125e0ad4067490b6f793562
seasouthern <matthew.james.johnson@hotmail.com>
2019-12-27 16:15:20 -0800

    Form is now taking data and putting into constructor function.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 
 console.log('js linked!');
 
+var takeout = [];
+
+// 27DEC 1613. Function is now taking in the data from the form. Next steps are to put the data into local storage and then pull the info out of local storage to render on the results page. The render function has been writen but is commented out.
+
 // Below is my constructor function.
 
 function Takeout(nameOfThisRestaraunt, walkTime, waitTime, price, rating) {
@@ -10,6 +14,7 @@ function Takeout(nameOfThisRestaraunt, walkTime, waitTime, price, rating) {
   this.waitTime = waitTime;
   this.price = price;
   this.rating = rating;
+  takeout.push(this);
 }
 
 
@@ -17,7 +22,7 @@ function Takeout(nameOfThisRestaraunt, walkTime, waitTime, price, rating) {
 
 function handleFormSubmitted(event) {
   event.preventDefault();
-  console.log(event);
+  console.log('event', event);
 
   var nameInput = document.getElementById('name');
   var nameValue = nameInput.value;
@@ -36,7 +41,7 @@ function handleFormSubmitted(event) {
 
   var newTakeout = new Takeout(nameValue, walkTimeValue, waitTimeValue, priceValue, ratingValue);
 
-  newTakeout.render();
+  // newTakeout.render();
 }
 
 var formElement = document.getElementById('new-takeout');
@@ -44,35 +49,34 @@ formElement.addEventListener('submit', handleFormSubmitted);
 
 // This is the function that will render the data table. I want to get the data to just render first before I work on adding to and displaying from Local Storage.
 
-Takeout.prototype.render = function() {
-  var takeoutTable = document.getElementById('takeout-table');
+// Takeout.prototype.render = function() {
+//   var takeoutTable = document.getElementById('takeout-table');
   
-  var takeoutRow = document.createElement('tr');
+//   var takeoutRow = document.createElement('tr');
   
-  var nameCell = document.createElement('td');
-  nameCell.textContent = this.name;
-  takeoutRow.appendChild(nameCell);
+//   var nameCell = document.createElement('td');
+//   nameCell.textContent = this.name;
+//   takeoutRow.appendChild(nameCell);
 
-  var walkTimeCell = document.createElement('td');
-  walkTimeCell.textContent = this.walkTime;
-  takeoutRow.appendChild(walkTimeCell);
+//   var walkTimeCell = document.createElement('td');
+//   walkTimeCell.textContent = this.walkTime;
+//   takeoutRow.appendChild(walkTimeCell);
 
-  var waitTimeCell = document.createElement('td');
-  waitTimeCell.textContent = this.waitTime;
-  takeoutRow.appendChild(waitTimeCell);
+//   var waitTimeCell = document.createElement('td');
+//   waitTimeCell.textContent = this.waitTime;
+//   takeoutRow.appendChild(waitTimeCell);
 
-  var priceCell = document.createElement('td');
-  priceCell.textContent = this.price;
-  takeoutRow.appendChild(priceCell);
+//   var priceCell = document.createElement('td');
+//   priceCell.textContent = this.price;
+//   takeoutRow.appendChild(priceCell);
 
-  var ratingCell = document.createElement('td');
-  ratingCell.textContent = this.rating;
-  takeoutRow.appendChild(ratingCell);
-};
+//   var ratingCell = document.createElement('td');
+//   ratingCell.textContent = this.rating;
+//   takeoutRow.appendChild(ratingCell);
+// };
 
-var takeout = [];
+// for (var i = 0; i <takeout.length; i++) {
+//   takeout[i].render();
+// }
 
-for (var i =0; i <takeout.length; i++) {
-  takeout[i].render();
-}
-
+console.log('takeout', takeout);

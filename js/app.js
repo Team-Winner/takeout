06cd843a1f484d4ae9f5a9051527315aee8bf6f3
John Cokos <highlander.44@gmail.com>
2020-01-14 11:21:56 -0800

    Code Review Comments from @johncokos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/*
+  CODE REVIEW: Avoid globals where you can.
+  This should be a static array on the Takeout constructor
+  i.e. Takeout.journalEntries = [] (after the constructor, of course)
+*/
+
 var takeout = [];
 
 // Below is our constructor function.
@@ -10,16 +16,33 @@ function Takeout(userName, resName, walkTime, waitTime, totalTime, price, rating
   this.walkTime = parseInt(walkTime, 10);
   this.waitTime = parseInt(waitTime, 10);
   this.totalTime = this.walkTime + this.waitTime + this.walkTime;
+  /*
+    CODE REVIEW: It's generally frowned upon to calculate values in the constructor
+    as you create a property. Better to have a Takeout.prototype.calculateTotalTime()
+    method that you call after you create the instance, as well as later should any
+    of those data points change
+  */
   this.price = price;
   this.rating = rating;
   takeout.push(this);
 }
-
 // This is the function that will take in the data entered in the form and put in the constructor function.
 
 function handleFormSubmitted(event) {
+  /*
+    CODE REVIEW
+    Given the event.preventDefault() here, you tried to force a redirect
+    in the HTML with that second button. Again, use a checkbox in the form
+    and then redirect in this function if the user checked it. Much cleaner
+  */
   event.preventDefault();
   console.log('event', event);
+
+  /*
+    CODE REVIEW
+    These can all be shortened to one-liners for clarity
+    var userNameValue = document.getElementById('user-name').value;
+  */
   var userNameInput = document.getElementById('user-name');
   var userNameValue = userNameInput.value;
 
@@ -32,6 +55,10 @@ function handleFormSubmitted(event) {
   var waitTimeInput = document.getElementById('wait-time');
   var waitTimeValue = waitTimeInput.value;
 
+  /*
+    CODE REVIEW:
+    What do you expect `this` referring to here?
+  */
   var totalTimeValue = this.totalTime;
 
   var priceInput = document.getElementById('price');
@@ -40,34 +67,58 @@ function handleFormSubmitted(event) {
   var ratingInput = document.getElementById('rating');
   var ratingValue = ratingInput.value;
 
+  /*
+    CODE REVIEW:
+    If you're not going to use the variable, you can just call the constructor function
+    JavaScript will keep that instance in memory for you
+  */
   var newTakeout = new Takeout(userNameValue, resNameValue, walkTimeValue, waitTimeValue, totalTimeValue, priceValue, ratingValue);
 
   // Below is sending data to local storage
-
   localStorage.setItem('savedTakeout', JSON.stringify(takeout));
-  for (var i = 0; i < takeout.length; i++);
 
+  /*
+    CODE REVIEW:
+    What is this for loop intended to do?
+  */
+  for (var i = 0; i < takeout.length; i++);
 
   document.getElementById('new-takeout').reset();
 }
 
 // Below is the function to pull the Takeout data from local storage.
 
+/*
+  CODE REVIEW
+  This is not actually a function as you note
+  Even though this will run on page load by the nature of JS running "top down"
+  It is still best practice to create a start() or initialize() function
+  that does this work, and then call it as the first executable line of code
+*/
+
 var savedTakeoutString = localStorage.getItem('savedTakeout');
 if (savedTakeoutString) {
   var arrayOfNotTakeout = JSON.parse(savedTakeoutString);
   for (var k = 0; k < arrayOfNotTakeout.length; k++) {
-    new Takeout(arrayOfNotTakeout[k].userName,arrayOfNotTakeout[k].resName, arrayOfNotTakeout[k].walkTime, arrayOfNotTakeout[k].waitTime, arrayOfNotTakeout[k].totalTime, arrayOfNotTakeout[k].price, arrayOfNotTakeout[k].rating);
+    new Takeout(arrayOfNotTakeout[k].userName, arrayOfNotTakeout[k].resName, arrayOfNotTakeout[k].walkTime, arrayOfNotTakeout[k].waitTime, arrayOfNotTakeout[k].totalTime, arrayOfNotTakeout[k].price, arrayOfNotTakeout[k].rating);
   }
 
 } else {
 
-  new Takeout('Mat','Subway', '5', '2', '12', '$', '★★★☆');
-  new Takeout('Sally','Mantra', '4', '10', '20', '$$', '★★★☆');
-  new Takeout('Keith','Japonesa', '20', '7', '47', '$$$', '★★★★');
-  new Takeout('Bella','7-11', '4', '2', '10', '$', '★☆☆☆');
-  new Takeout('Drew','Buffalo Wild Wings', '16', '2', '34', '$$', '★★☆☆');
+  new Takeout('Mat', 'Subway', '5', '2', '12', '$', '★★★☆');
+  new Takeout('Sally', 'Mantra', '4', '10', '20', '$$', '★★★☆');
+  new Takeout('Keith', 'Japonesa', '20', '7', '47', '$$$', '★★★★');
+  new Takeout('Bella', '7-11', '4', '2', '10', '$', '★☆☆☆');
+  new Takeout('Drew', 'Buffalo Wild Wings', '16', '2', '34', '$$', '★★☆☆');
 }
+
+/*
+  CODE REVIEW
+  It's a clean practice to ut all global variable declarations at the top of
+  the file and all executing code (event listeners) here at the bottom.
+
+  Make sure you're consistent about it.
+*/
 
 var formElement = document.getElementById('new-takeout');
 formElement.addEventListener('submit', handleFormSubmitted);
--- js/results.js
+++ js/results.js
@@ -4,6 +4,13 @@ var takeout = [];
 
 // Below is our constructor function.
 
+/*
+  CODE REVIEW
+  Why the repeated code here in both modules?
+  If you make one "amazing" js file with the constructor function and
+  prototypes, both the results page and the home page can use it just the same,
+  giving you only one place to write the logical code
+*/
 function Takeout(userName, resName, walkTime, waitTime, totalTime, price, rating) {
   this.userName = userName;
   this.resName = resName;
@@ -18,26 +25,37 @@ function Takeout(userName, resName, walkTime, waitTime, totalTime, price, rating
 
 // Below is the function to pull the Takeout data from local storage.
 
+/*
+  CODE REVIEW
+  Again, same comments as above, this code is repeated from the other .js file
+  Make your common functionality in one file and both pages can use it
+
+  Then, on each page, you only need a small .js file that USES those functions
+  as they need to.
+
+  This will make your site far easier to manage and maintain
+*/
+
 var savedTakeoutString = localStorage.getItem('savedTakeout');
 if (savedTakeoutString) {
   var arrayOfNotTakeout = JSON.parse(savedTakeoutString);
   for (var k = 0; k < arrayOfNotTakeout.length; k++) {
-    new Takeout(arrayOfNotTakeout[k].userName,arrayOfNotTakeout[k].resName, arrayOfNotTakeout[k].walkTime, arrayOfNotTakeout[k].waitTime, arrayOfNotTakeout[k].totalTime, arrayOfNotTakeout[k].price, arrayOfNotTakeout[k].rating);
+    new Takeout(arrayOfNotTakeout[k].userName, arrayOfNotTakeout[k].resName, arrayOfNotTakeout[k].walkTime, arrayOfNotTakeout[k].waitTime, arrayOfNotTakeout[k].totalTime, arrayOfNotTakeout[k].price, arrayOfNotTakeout[k].rating);
   }
 
 } else {
 
-  new Takeout('Mat','Subway', '5', '2', '12', '$', '★★★☆');
-  new Takeout('Sally','Mantra', '4', '10', '20', '$$', '★★★☆');
-  new Takeout('Keith','Japonesa', '20', '7', '47', '$$$', '★★★★');
-  new Takeout('Bella','7-11', '4', '2', '10', '$', '★☆☆☆');
-  new Takeout('Drew','Buffalo Wild Wings', '16', '2', '34', '$$', '★★☆☆');
+  new Takeout('Mat', 'Subway', '5', '2', '12', '$', '★★★☆');
+  new Takeout('Sally', 'Mantra', '4', '10', '20', '$$', '★★★☆');
+  new Takeout('Keith', 'Japonesa', '20', '7', '47', '$$$', '★★★★');
+  new Takeout('Bella', '7-11', '4', '2', '10', '$', '★☆☆☆');
+  new Takeout('Drew', 'Buffalo Wild Wings', '16', '2', '34', '$$', '★★☆☆');
 }
 console.log('takeout', takeout);
 
 // This is the function that will render the data table.
 
-Takeout.prototype.render = function() {
+Takeout.prototype.render = function () {
   var takeoutTableBody = document.getElementById('takeout-table-body');
 
   var takeoutRow = document.createElement('tr');
@@ -78,46 +96,63 @@ Takeout.prototype.render = function() {
 
 };
 
-for (var i = 0; i <takeout.length; i++) {
+for (var i = 0; i < takeout.length; i++) {
   takeout[i].render();
 }
 
 // Below are the functions for sorting takeout array based on different clicks on the table headers.
 
-function sortByWalkTime (event) {
+/*
+  CODE REVIEW
+
+  In each sort method, you're performing 2 tasks.
+    1. The sort itself
+    2. A re-rendering of the table
+
+  Since each of them re-renders the table the exact same way, that should
+  probably be a function of its own that gets called following the sort.
+
+  The sort functions should not call that re-render function. The click
+  handler that you should be using for the table headers should do both:
+  Call the correct sort function, then the re-render.  That will be very
+  clean.
+*/
+
+function sortByWalkTime(event) {
   takeout.sort(function (a, b) {
     return a.walkTime - b.walkTime;
   });
+
   var takeoutTableBody = document.getElementById('takeout-table-body');
   takeoutTableBody.innerHTML = '';
-  for (var i = 0; i <takeout.length; i++) {
+  for (var i = 0; i < takeout.length; i++) {
     takeout[i].render();
   }
 }
 
-function sortByWaitTime (event) {
+function sortByWaitTime(event) {
   takeout.sort(function (a, b) {
     return a.waitTime - b.waitTime;
   });
   var takeoutTableBody = document.getElementById('takeout-table-body');
   takeoutTableBody.innerHTML = '';
-  for (var i = 0; i <takeout.length; i++) {
+  for (var i = 0; i < takeout.length; i++) {
     takeout[i].render();
   }
 }
 
-function sortByTotalTime (event) {
+function sortByTotalTime(event) {
   takeout.sort(function (a, b) {
     return a.totalTime - b.totalTime;
   });
   var takeoutTableBody = document.getElementById('takeout-table-body');
   takeoutTableBody.innerHTML = '';
-  for (var i = 0; i <takeout.length; i++) {
+  for (var i = 0; i < takeout.length; i++) {
     takeout[i].render();
   }
 }
 
-function sortByPrice (event) {
+function sortByPrice(event) {
   takeout.sort(function (a, b) {
     if (a.price.length > b.price.length) {
       return 1;
@@ -128,12 +163,12 @@ function sortByPrice (event) {
   });
   var takeoutTableBody = document.getElementById('takeout-table-body');
   takeoutTableBody.innerHTML = '';
-  for (var i = 0; i <takeout.length; i++) {
+  for (var i = 0; i < takeout.length; i++) {
     takeout[i].render();
   }
 }
 
-function sortByRating (event) {
+function sortByRating(event) {
   takeout.sort(function (a, b) {
     if (a.rating > b.rating) {
       return 1;
@@ -145,7 +180,7 @@ function sortByRating (event) {
 
   var takeoutTableBody = document.getElementById('takeout-table-body');
   takeoutTableBody.innerHTML = '';
-  for (var i = 0; i <takeout.length; i++) {
+  for (var i = 0; i < takeout.length; i++) {
     takeout[i].render();
   }
 }
